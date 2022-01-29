e25e1dbd85c5e1d5b1140111c6bcbf2538dc4f4c
nguyenquannnn <nguyenquan233@gmail.com>
2022-01-29 13:07:51 -0500

    Add wiki modules

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def helloWorld():
+  return "Hello, cross-origin-world!"
 
 
 @app.route('/v1/query')
 def 
+
+
+def set_user_agent():
+    ini_set('user_agent', 'MyCoolTool/1.1 (https://example.org/MyCoolTool/; MyCoolTool@example.org) UsedBaseLibrary/1.4');
---
+++ backend/wiki.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import urllib.parse
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+def query_search_string(query_string):
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "list": "search",
+        "srsearch": query_string
+    }
+
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+
+    if (DATA["query"] and DATA["query"]["search"]):
+        return [ {
+            "title": i["title"],
+            "pageid": i["pageid"]
+        } for i in DATA["query"]["search"] ]
+    else:
+        return []
+
+
+def fetch_page_content(page_title):
+    PARAMS = {
+        "action": "query",
+        "formatversion": "2",
+        "rvprop": "content",
+        "prop": "revisions",
+        "rvslots": "*",
+        "titles": page_title
+    }
+
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+
+def generate_wiki_page_url(pageid):
+    return "https://en.wikipedia.org/?curid={pageid}".format(pageid)
+
+def query_page_with_table(query_string):
+    PARAMS = {
+        'action': 'query',
+        'list': 'embeddedin',
+        'eititle': 'Template:Table',
+        "srsearch": query_string,
+        'format': 'json'
+    }
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    print(DATA)
+
+if __name__ == '__main__':
+    print(query_search_string("chess grandmasters"))
+    # print(query_search_string("List of chess grandmasters"))
+    # print(fetch_page_content("lol"))
